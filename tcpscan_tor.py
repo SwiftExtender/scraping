e0b12b4c0461c9e39ad7e71097e066f893ff5fa8
Wowhere <69252550+Wowhere@users.noreply.github.com>
2023-08-22 12:50:33 +0300

    Update and rename tcpscan_tor to tcpscan_tor.py

---
+++ tcpscan_tor.py
@@ -0,0 +1,57 @@
+import socks  # SocksiPy module
+import socket
+import sys
+import datetime
+import html_logger
+
+def gen_timestamp():
+    return datetime.now().strftime('%d %H:%M:%S')
+
+html_logger.setup('Tot ports results', filename=gen_timestamp()+'_results.html', version="0.0.1")
+
+SOCKS_PORT = 9050
+
+# Set socks proxy
+
+socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, '127.0.0.1', SOCKS_PORT)
+socket.socket = socks.socksocket
+
+# Perform DNS resolution through the socket
+
+def getaddrinfo(*args):
+    return [(socket.AF_INET, socket.SOCK_STREAM, 6, '', (args[0], args[1]))]
+
+socket.getaddrinfo = getaddrinfo
+
+def check_port(h, p):
+    try:
+        s = socks.socksocket()
+        s.settimeout(7)
+        s.connect((h, p))
+        message = b'GET / HTTP/1.0\r\n\r\n'
+        s.sendall(message)
+        res = s.recv(8192)
+        success = h+":"+str(p)+" opened"
+        html_logger.dbg(gen_timestamp())
+        html_logger.dbg(success)
+        html_logger.dbg(res)
+    except Exception as e:
+        err = h+":"+str(p)+" no connect"
+        html_logger.dbg(gen_timestamp())
+        html_logger.dbg(h+":"+str(p)+" no connect")
+        html_logger.dbg(e)
+
+ports_file = open("!assets\\ports.txt")
+ports = ports_file.readlines()
+ports_file.close()
+
+if sys.argv[2] == "list":
+    for port in ports:
+        check_port(sys.argv[1], int(port.rstrip()))
+
+elif sys.argv[2] == "all":
+    for port in range(0, 65535):
+        check_port(sys.argv[1], port)
+
+else:
+    print("Wrong option for scan")
